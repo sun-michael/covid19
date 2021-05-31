871a8c337c8b970e861a9faf47a0dff41b651a34
Michael Sun <sun.m710@gmail.com>
2021-05-31 17:51:50 -0500

    adding files for github pages

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,8 +6,6 @@ import './card-list.styles.css';
 
 export const CardList = (props) => (
   <div className="card-list">
-    {console.log("index is ")}
-    {console.log(props)}
     {props.usStates.map((s, index) => (
       < Card key={index} usState={s} />
     ))}
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,5 +6,8 @@ export const Card = (props) => (
   <div className="card-container">
     <h1>{props.usState.province}</h1>
     <h3>{props.usState.county}</h3>
+    <h4>confirmed: {props.usState.stats.confirmed}</h4>
+    <h4>deaths: {props.usState.stats.deaths}</h4>
+    <h4>last update: {props.usState.updatedAt}</h4>
   </div>
 )
